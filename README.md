7d382e6a212cf795c1960e01d1fd1fab244ad894
skullkid61 <43558378+skullkid61@users.noreply.github.com>
2020-10-09 14:28:18 +1030

    got the keyup function working

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ In this project I've been practising using the command line in VSCode to create 
 ## Resources
 
 Using [makeareadme.com](https//makeareadme.com) I've looked at best practice for creating markdown files
+
+## Notes about test
+
+In this project I'm practising moving the results of one array to a new array based on wheter the array items match an input from the user. This is to solve the problem of appending student results in another project. The idea is that rather than change whether the student results display on the page or are hidden, to change the data in the array first and then append the results. This means that it's not pulling in all the data onto the page and then hiding it, it's actually not pulling the info in the first place if it's not relevant.
--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-const arr1 = ['Dace', 'Matt', 'Maddy', 'Scott'];
-const arr2 = [];
+const ul = document.querySelector('ul');
+const arr1 = ['Dace', 'Matt', 'Maddy', 'Scott', 'Phil', 'Zane', 'Jamie'];
+const search = document.querySelector('[type]')
+let targetFriendList = '';
 
 // create a function that takes items from first array and then shifts them into the second array based on search criteria
+
+// let input = prompt('What names should come through into the second array?')
+
+
+function newResults(list, input) {
+	let newArray = [];
+	targetFriendList = '';
+	input = input.toLowerCase();
+	for (i=0; i < list.length; i++){
+		if (list[i].toLowerCase().indexOf(input) > -1) {
+			newArray.push(list[i]);
+		}
+	}
+	for (i=0; i < newArray.length; i++) {
+		targetFriendList += `<li>${newArray[i]}</li>`
+	}
+	return targetFriendList;
+}
+
+search.addEventListener('keyup', () => {
+	ul.innerHTML = '';
+	input = search.value;
+	newResults(arr1, input);
+	ul.insertAdjacentHTML('beforeend', targetFriendList);
+})
